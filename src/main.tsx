f4d8b599e570ac13192de91102d48d679b46ecb4
Ralibali <36996821+Ralibali@users.noreply.github.com>
2026-08-15 22:41:20 +0200

    feat: load Äggboden v4 styles

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import "./honsgarden-core-v3.css";
 import "./honsgarden-yard-v3.css";
 import "./honsgarden-egg-v3.css";
 import "./honsgarden-delight-v4.css";
+import "./honsgarden-sales-v4.css";
 import "./i18n"; // initierar i18next (sv + en) före render
 import { installGlobalErrorHandlers } from "@/lib/errorLogger";
 import { isStandalonePwa, recoverStalePwaShell } from "@/lib/pwaUpdate";
