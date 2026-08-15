044e1f486f456a766137d5d53f061674ff3c7daa
Ralibali <36996821+Ralibali@users.noreply.github.com>
2026-08-16 01:37:41 +0200

    fix: load single-scene Today layout last

--- src/main.tsx
+++ src/main.tsx
@@ -18,6 +18,7 @@ import "./honsgarden-routines-v5.css";
 import "./honsgarden-settings-v5.css";
 import "./honsgarden-mobile-v5.css";
 import "./honsgarden-atmosphere-v6.css";
+import "./honsgarden-today-v7.css";
 import "./i18n"; // initierar i18next (sv + en) före render
 import { installGlobalErrorHandlers } from "@/lib/errorLogger";
 import { installFarmAtmosphereRuntime } from "@/lib/farmAtmosphereRuntime";
